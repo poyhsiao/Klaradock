38cd7579592de4dc59d1cb06878128b131e6cc80
Kim Hsiao <white.little@gmail.com>
2017-05-19 11:46:07 +0800

    - remove jenkins and certbot from start.sh / build.sh / rebuild.sh by default - Add gitignore file for caddy and certbot that should copy and generate the customize setting according the request - Fix mongodb extension setting for php-fpm and workspace - remove sqlsrv.so and pdo_sqlsrv.so from php-fpm laravel.ini setting - Update opcache.ini that should be better suitable for development

--- rebuild.sh
+++ rebuild.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker-compose build --no-cache --pull --force-rm workspace nginx mysql php-fpm php-worker redis jenkins certbot
+docker-compose build --no-cache --pull --force-rm workspace caddy mysql php-fpm php-worker redis
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-$(which docker-compose) up -d workspace caddy mysql php-fpm php-worker redis jenkins certbot
+$(which docker-compose) up -d workspace caddy mysql php-fpm php-worker redis
